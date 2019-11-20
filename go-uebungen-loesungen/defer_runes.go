075af126ba2b502d8831c7ecd317ca72cbfd4c87
Maximilian Broszio <48601535+maxbroszio@users.noreply.github.com>
2019-11-20 12:31:46 +0100

    Update defer_runes.go
    Lösung per Text beschrieben

--- go-uebungen-loesungen/defer_runes.go
+++ go-uebungen-loesungen/defer_runes.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 )
-
+/*
+	Das defer Schlüsselwort sorgt dafür, dass die danach folgenden Anweisungen auf den Stack gelegt und erst nach
+	beenden der umschließenden Funktion via dem LIFO-Prinzip (Last in First out) ausgeführt werden. In diesem 
+	Beispiel wird erst jedes Printf Statement auf den Stack gelegt und am Ende der Funktion beginnend mit dem  
+	LETZTEN Printf Statment der Stack abgearbeitet. Dadurch wird die Ausgabe des übergebenen Arrays umgedreht.
+*/
 func flipText(arr []rune) {
 	fmt.Println("Der Text umgedreht lautet:")
 	for i := 0; i < len(arr); i++ {
