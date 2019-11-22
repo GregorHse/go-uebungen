d0bac591cc38b9d13ed48d78700d2b38db9411ff
unidragon58 <48785622+unidragon58@users.noreply.github.com>
2019-11-22 15:51:14 +0100

    structs & pointer

---
+++ go-uebungen/pointer.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+	Geben Sie mithilfe eines Pointers sowohl den Wert, als auch die Adresse an der val gespeichert ist
+	auf die Konsole aus.
+*/
+
+func main() {
+
+	val := 9
+
+}
---
+++ go-uebungen/structs.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+	Erstellen Sie ein Struct Studierender, dass Felder für Vor- & Nachnamen, Matrikelnummer und
+	Studiengang enthält. Initiieren Sie eine Instanz des Structs mit den Werten des Informatikstudenten
+	Peter Müller, der die Matrikelnummer 1234567 hat.
+	Ändern Sie den Studiengang mithilfe eines Pointers von Informatik auf Wirtschaftsinformatik.
+*/
+
+func main() {
+
+}
